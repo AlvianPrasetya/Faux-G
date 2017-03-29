ebc87ce87076f837aadee389ac692957aae5c1de
Alvian Prasetya <prasetyaa@garena.com>
2017-03-29 16:50:28 +0800

    Refactor weapon parameters and add reload volume parameter

--- Assets/Faux-G/Scripts/Game/Weapon.cs
+++ Assets/Faux-G/Scripts/Game/Weapon.cs
@@ -4,31 +4,37 @@ public class Weapon : MonoBehaviour {
 
 	public Sprite crosshairSprite;
 	public Vector2 crosshairSize;
-	public Vector3 weaponMeshScale;
-	public Vector3 weaponMeshRotation;
+
 	public Mesh weaponMesh;
 	public Material weaponMaterial;
-	public ProjectileController prefabBullet;
-	public AudioClip fireSound;
-	public float fireVolume;
-	public AudioClip reloadSound;
 
-	public float cooldown;
-	public int ammo;
-
-	public float toggleAimTime;
+	public Vector3 weaponMeshScale;
+	public Vector3 weaponMeshRotation;
 
 	public Vector3 weaponPosition;
-	public Vector3 weaponMuzzlePosition;
+	public Vector3 aimWeaponPosition;
+
 	public Vector3 cameraPosition;
 	public float cameraFieldOfView;
-	public Vector3 aimWeaponPosition;
+
 	public Vector3 aimCameraPosition;
 	public float aimCameraFieldOfView;
 
-	public float changeWeaponTime;
+	public Vector3 weaponMuzzlePosition;
 
+	public float cooldown;
+	public int ammo;
+	public float toggleAimTime;
+	public float changeWeaponTime;
 	public float reloadTime;
+
+	public ProjectileController prefabBullet;
+
+	public AudioClip fireSound;
+	public float fireVolume;
+
+	public AudioClip reloadSound;
+	public float reloadVolume;
 
 	// The min/max recoil angle (degrees/projectile) in horizontal and vertical directions
 	public Vector2 minRecoil;
--- Assets/Faux-G/Scripts/Game/WeaponController.cs
+++ Assets/Faux-G/Scripts/Game/WeaponController.cs
@@ -344,6 +344,7 @@ public class WeaponController : Photon.MonoBehaviour {
 
 	private void LocalReload(int weaponId) {
 		if (weapons[weaponId].reloadSound != null) {
+			audioSource.volume = weapons[weaponId].reloadVolume;
 			audioSource.PlayOneShot(weapons[weaponId].reloadSound);
 		}
 	}
