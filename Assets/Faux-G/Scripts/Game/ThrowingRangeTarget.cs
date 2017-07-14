f8ef054d47a6ef60812459d0f9395036df54f2bb
unknown <alvian.better@gmail.com>
2017-07-14 18:34:17 +0700

    Implement networked rotation on throwing range target

--- Assets/Faux-G/Scripts/Game/ThrowingRangeTarget.cs
+++ Assets/Faux-G/Scripts/Game/ThrowingRangeTarget.cs
@@ -1,10 +1,16 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class ThrowingRangeTarget : MonoBehaviour {
 
     public delegate void OnTargetHitCallback(PhotonPlayer hittingPlayer, int points);
 
+    public float maxRotateSpeed;
+    public float changeAngularVelocityInterval;
+
     public HitArea[] hitAreas;
+
+    private Vector3 angularVelocity;
 
     private OnTargetHitCallback hitCallback;
 
@@ -20,8 +26,33 @@ public class ThrowingRangeTarget : MonoBehaviour {
         }
     }
 
+    void Start() {
+        if (PhotonNetwork.isMasterClient) {
+            StartCoroutine(ChangeAngularVelocityCoroutine());
+        }
+    }
+
+    private void FixedUpdate() {
+        if (PhotonNetwork.isMasterClient) {
+            transform.Rotate(transform.right, angularVelocity.x * Time.fixedDeltaTime, Space.World);
+            transform.Rotate(transform.up, angularVelocity.y * Time.fixedDeltaTime, Space.World);
+            transform.Rotate(transform.forward, angularVelocity.z * Time.fixedDeltaTime, Space.World);
+        }
+    }
+
     private void OnHitAreaHit(PhotonPlayer hittingPlayer, int points) {
         hitCallback(hittingPlayer, points);
+    }
+
+    private IEnumerator ChangeAngularVelocityCoroutine() {
+        while (true) {
+            angularVelocity = new Vector3(
+                Random.Range(-maxRotateSpeed, maxRotateSpeed),
+                Random.Range(-maxRotateSpeed, maxRotateSpeed),
+                Random.Range(-maxRotateSpeed, maxRotateSpeed));
+
+            yield return new WaitForSeconds(changeAngularVelocityInterval);
+        }
     }
 
 }
