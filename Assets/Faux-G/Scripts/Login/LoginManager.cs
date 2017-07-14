735e88bf9db5953c65bd9a5aa4b38da15c911fc2
unknown <alvian.better@gmail.com>
2017-07-14 17:12:04 +0700

    Implement player naming capability at login screen

--- Assets/Faux-G/Scripts/Login/LoginManager.cs
+++ Assets/Faux-G/Scripts/Login/LoginManager.cs
@@ -3,17 +3,23 @@ using UnityEngine.UI;
 
 public class LoginManager : Photon.PunBehaviour {
 
+    public Text playerNameText;
+    public InputField playerNameField;
+    public Button loginButton;
 	public Text loginStatusText;
+
+    private string confirmedPlayerName;
 
 	void Awake() {
 		PhotonNetwork.logLevel = PhotonLogLevel.ErrorsOnly;
 		PhotonNetwork.autoJoinLobby = false;
-	}
 
-	void Start() {
-		Logger.Log("Connecting to Server");
-		loginStatusText.text = "Connecting to Server";
-		PhotonNetwork.ConnectUsingSettings(Utils.GAME_VERSION);
+        playerNameText.gameObject.SetActive(true);
+        playerNameField.gameObject.SetActive(true);
+        loginButton.gameObject.SetActive(true);
+        loginStatusText.gameObject.SetActive(false);
+
+        loginButton.onClick.AddListener(Connect);
 	}
 
 	public override void OnConnectedToPhoton() {
@@ -29,7 +35,25 @@ public class LoginManager : Photon.PunBehaviour {
 
 	public override void OnJoinedLobby() {
 		Logger.Log("Joined Lobby");
-		SceneManager.LoadScene(Utils.Scene.LOBBY);
+
+        // Change networked name to the confirmed player name
+        PhotonNetwork.playerName = confirmedPlayerName;
+
+        // Load lobby
+        SceneManager.LoadScene(Utils.Scene.LOBBY);
 	}
+
+    private void Connect() {
+        confirmedPlayerName = playerNameField.text;
+
+        playerNameText.gameObject.SetActive(false);
+        playerNameField.gameObject.SetActive(false);
+        loginButton.gameObject.SetActive(false);
+        loginStatusText.gameObject.SetActive(true);
+
+        Logger.Log("Connecting to Server");
+        loginStatusText.text = "Connecting to Server";
+        PhotonNetwork.ConnectUsingSettings(Utils.GAME_VERSION);
+    }
 
 }
